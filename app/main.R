e043c6f71dd20dbb150bcbdbf2fc7f70703c85d7
davidrsch <daviddrsch@gmail.com>
2025-03-22 18:50:32 +0100

    Modifying app main

--- app/main.R
+++ app/main.R
@@ -1,25 +1,55 @@
 box::use(
-  shiny[bootstrapPage, div, moduleServer, NS, renderUI, tags, uiOutput],
+  shiny.fluent[fluentPage, Pivot, PivotItem],
+  shiny[moduleServer, NS],
+)
+
+box::use(
+  app / view / home / home,
 )
 
 #' @export
 ui <- function(id) {
   ns <- NS(id)
-  bootstrapPage(
-    uiOutput(ns("message"))
+  # Use CommandBar to mimic the top horizontal bar
+  fluentPage(
+    Pivot(
+      PivotItem(
+        headerText = "Home",
+        home$ui(id = ns("home"))
+      ),
+      # SEARCH tab (placeholder)
+      PivotItem(
+        headerText = "Search",
+        # You can call another module if you have one, e.g. search$ui(...)
+        "Search page content goes here..."
+      ),
+      # DISCLAIMER tab (placeholder)
+      PivotItem(
+        headerText = "Disclaimer",
+        "Disclaimer page content goes here..."
+      ),
+      # REPORT PROBLEM tab (placeholder)
+      PivotItem(
+        headerText = "Report a Problem",
+        "Report a Problem page content goes here..."
+      ),
+      # PRINT tab (placeholder)
+      PivotItem(
+        headerText = "Print",
+        "Print page content goes here..."
+      ),
+      # FEEDBACK tab (placeholder)
+      PivotItem(
+        headerText = "Site Feedback",
+        "Site Feedback page content goes here..."
+      )
+    )
   )
 }
 
 #' @export
 server <- function(id) {
   moduleServer(id, function(input, output, session) {
-    output$message <- renderUI({
-      div(
-        style = "display: flex; justify-content: center; align-items: center; height: 100vh;",
-        tags$h1(
-          tags$a("Check out Rhino docs!", href = "https://appsilon.github.io/rhino/")
-        )
-      )
-    })
+    home$server("home")
   })
 }
