2e4a682477dc6f0d266309a60477195283f0acef
Julian Guillo <julianleioa@gmail.com>
2025-03-22 18:34:45 +0100

    Added title, table and plot

--- R/app_server.R
+++ R/app_server.R
@@ -3,7 +3,67 @@
 #' @param input,output,session Internal parameters for {shiny}.
 #'     DO NOT REMOVE.
 #' @import shiny
+#' @importFrom dplyr select filter between
+#' @importFrom tidyr pivot_longer
+#' @importFrom plotly renderPlotly plot_ly layout config
+#' @importFrom DT renderDT datatable
 #' @noRd
 app_server <- function(input, output, session) {
-  # Your application server logic
+  
+  # Title-----------------------------------------------------------------------
+  # Render the card title dynamically based on the selected chart type
+  output$table_title <- renderText({
+    req(input$chart_type)  # Make sure input is available
+    
+    # Update the title based on selected chart type
+    chart_titles <- c(
+      "by_reporter_type" = "Reports received by Reporter Type",
+      "by_reporter" = "Reports received by Reporter",
+      "by_reporter_region" = "Reports received by Reporter Region",
+      "by_report_seriousness" = "Reports received by Report Seriousness",
+      "by_age_group" = "Reports received by Age Group",
+      "by_sex" = "Reports received by Sex"
+    )
+    
+    # Return the title based on the selected chart type
+    chart_titles[input$chart_type]
+  })
+  
+  # Table-----------------------------------------------------------------------
+  output$table <- renderDT({
+    req(input$chart_type)  # Make sure input is available
+    
+    # Get the columns to use based on the selected chart type
+    columns_to_use <- get_plot_columns(input$chart_type)
+    
+    # Filter the data to include only the specified columns (and 'year')
+    table_data <- mock_data %>%
+      select(c("year", "total", columns_to_use))
+    
+    # Create the datatable
+    datatable(
+      table_data, 
+      options = list(
+        autoWidth = TRUE,
+        scrollY = "300px",      # Set height for vertical scrolling
+        scrollCollapse = TRUE,  # Collapse the table when shorter than scrollY
+        paging = FALSE,         # Disable pagination
+        info = FALSE,
+        columnDefs = list(
+          list(width = "100px", targets = 0)
+        )
+      )
+    )
+  })
+  
+  # Stacked Bards Plot----------------------------------------------------------
+  output$stacked_bars <- renderPlotly({
+    req(input$chart_type)  # Make sure input is available
+    
+    # Get the columns to use based on the selected chart type
+    columns_to_use <- get_plot_columns(input$chart_type)
+    
+    # Create the stacked bars plot
+    stacked_bars(columns_to_use)
+  })
 }
--- R/app_ui.R
+++ R/app_ui.R
@@ -1,15 +1,71 @@
 #' The application User-Interface
-#'
-#' @param request Internal parameter for `{shiny}`.
+#' 
+#' @param request Internal parameter for `{shiny}`. 
 #'     DO NOT REMOVE.
 #' @import shiny
+#' @import bslib
+#' @importFrom golem add_resource_path activate_js favicon bundle_resources
+#' @importFrom htmltools tagList
+#' @importFrom DT DTOutput
+#' @importFrom plotly plotlyOutput
 #' @noRd
 app_ui <- function(request) {
   tagList(
+    # Leave this function for adding external resources
     golem_add_external_resources(),
-    fluidPage(
-      title = "My Golem App",
-      mod_simple_ui("simple_1")
+    
+    # Your page using bslib navbar layout
+    bslib::page_navbar(
+      title = "FAERS Data Explorer",
+      # Main dashboard tab
+      bslib::nav_panel(
+        title = "Dashboard",
+        
+        # divide the page into two columns
+        fluidRow(
+          column(width = 6,
+                 # Chart type selector in the main area
+                 selectInput(
+                   inputId = "chart_type",
+                   label = "Select Data Category:",
+                   choices = c(
+                     "By Reporter Type" = "by_reporter_type",
+                     "By Reporter" = "by_reporter",
+                     "By Reporter Region" = "by_reporter_region", 
+                     "By Report Seriousness" = "by_report_seriousness",
+                     "By Age Group" = "by_age_group",
+                     "By Sex" = "by_sex"
+                   ),
+                   selected = "by_reporter_type",
+                   width = "100%"
+                 )
+                 # table output in the left column
+          ),
+          column(width = 6)
+        ),
+        fluidRow(
+          column(width = 12,
+                 # chart output in the right column
+                 bslib::card(
+                   # Dynamically update the card title based on chart type selection
+                   bslib::card_header(
+                     textOutput("table_title")  # Place the textOutput in the card_header
+                   ),
+                   bslib::card_body(
+                     fluidRow(
+                       column(width = 6,
+                              DTOutput("table")
+                       ),
+                       column(width = 6,
+                              plotlyOutput("stacked_bars")
+                       )
+                     )
+                   )
+                 )
+                 
+          )
+        )
+      )
     )
   )
 }
@@ -27,14 +83,12 @@ golem_add_external_resources <- function() {
     "www",
     app_sys("app/www")
   )
-
+  
   tags$head(
     favicon(),
     bundle_resources(
       path = app_sys("app/www"),
       app_title = "shinyHackathon"
     )
-    # Add here other external resources
-    # for example, you can add shinyalert::useShinyalert()
   )
 }
